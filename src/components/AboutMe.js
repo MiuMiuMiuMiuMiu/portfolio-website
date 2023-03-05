309cd7236c41e505e4ce4e9fc57feae52a624d63
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-03-05 23:44:30 +0100

    Updates

--- src/components/AboutMe.js
+++
@@ -1,9 +0,0 @@
-function AboutMe() {
-    return (
-        <div id="about-me">
-            <h1>About me</h1>
-        </div>
-    )
-}
-
-export default AboutMe;
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div id="footer">
-            <p>Made by Thiptida Luengprakarn</p>
-        </div>
-    )
-}
-
-export default Footer;
