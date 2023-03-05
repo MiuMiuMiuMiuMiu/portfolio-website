d5cf92f668f9a046cffef45598edaf4eb4ff33cc
Thippi <37799874+MiuMiuMiuMiuMiu@users.noreply.github.com>
2023-03-05 15:12:18 +0100

    Change color on header

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -32,27 +32,15 @@ function Header(props) {
             {width >= 720
                 ?
                 <div className="navigation">
-                    <a href="#skills">About me</a>
+                    <a href="#about-me">About me</a>
                     <a href="#skills">Skills</a>
                     <a href="#projects">Projects</a>
                     <a href="#contact">Contact</a>
-                    <a href="https://github.com/MiuMiuMiuMiuMiu" className="float-right">
-                        <img className="icon" src="/github-logo.svg" alt="Github icon" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/thiptida-luengprakarn/" className="float-right">
-                        <img className="icon" src="/linkedIn-logo.png" alt="Github icon" />
-                    </a>
                 </div>
                 :
                 <>
                     <a href="#burgerMenu" className="burger-menu float-right" onClick={toggleNav}>
                         <i className="fa fa-bars"></i>
-                    </a>
-                    <a href="https://github.com/MiuMiuMiuMiuMiu" className="float-right">
-                        <img className="icon" src="/github-logo.svg" alt="Github icon" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/thiptida-luengprakarn/" className="float-right">
-                        <img className="icon" src="/linkedIn-logo.png" alt="Github icon" />
                     </a>
                     {/* If  width is not > or = 720 and hamburger menu is open, display nav */}
                     {open &&
@@ -63,6 +51,7 @@ function Header(props) {
                             <a href="#contact">Contact</a>
                         </div>
                     }
+                    
                 </>
             }
 
